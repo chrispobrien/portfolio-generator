bcebf87764a21345028be4e961f5e76469b1b267
chrispobrien <chris.obrien57@gmail.com>
2022-06-05 21:17:46 -0400

    9.5.7 Finished draft

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
+
 const mockData = {
   name: 'Lernantino',
   github: 'lernantino',
@@ -177,8 +178,35 @@ console.log(pageHTML);
 //   .then(promptProject)
 //   .then(portfolioData => {
 //     const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
-  // });
+// fs.writeFile('./dist/index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
+//   console.log('Page created! Check out index.html in this directory to see it!');
+//   });
+// // });
+
+// fs.copyFile('./src/style.css', './dist/style.css', err => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log('Style sheet copied successfully!');
+// });
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
